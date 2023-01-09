53fd7e1bdd3e3dbc642bf04e164975c52816661b
jeancarlos3197 <carlos.ademir_66@hotmail.com>
2023-01-09 17:37:09 -0500

    Inicio de copia de datos solo main

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:eje0802/application/use_cases/frmPrincipal.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: frmPrincipal(),
+    );
+  }
+}
